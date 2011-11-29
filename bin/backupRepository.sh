679244bc152a3cf62182a185f3c5fdcd65457b25
Frederic Bergeron <fbergeron@users.sourceforge.net>
2011-11-29 17:21:55 +0900

    Removed obsolete backupRepository.sh script.

--- bin/backupRepository.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo Enter latest revision number.
-read rev
-mkdir javasol-r$rev-svn-backup
-rsync -av javasol.svn.sourceforge.net::svn/javasol/* javasol-r$rev-svn-backup
-tar zcf javasol-r$rev-svn-backup.tar.gz javasol-r$rev-svn-backup
-rm -rf javasol-r$rev-svn-backup

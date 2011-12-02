298aba80ef6277175fad70f7a5651c482186fad9
Frederic Bergeron <fbergeron@users.sourceforge.net>
2011-12-02 21:07:11 +0900

    Simplified release script.

--- bin/release.sh
+++ bin/release.sh
@@ -2,12 +2,7 @@ echo "Enter release (e.g.: 1.4): "
 read rel
 echo $rel
 
-mkdir javasol-$rel
-cp dist/javasol/index.html javasol-$rel
-mkdir javasol-$rel/jar
-mkdir javasol-$rel/img
-cp dist/javasol/jar/*.jar javasol-$rel/jar
-cp dist/javasol/img/*.* javasol-$rel/img
-tar -cf javasol-$rel.tar javasol-$rel
-gzip javasol-$rel.tar
+cp -r dist/javasol javasol-$rel
+tar zcf javasol-$rel.tar.gz javasol-$rel
+rm -rf javasol-$rel
 exit 0
